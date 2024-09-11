fa388ee65ee02a875b8c653de8cdb1f38d3fa14e
Ikhsan <ikhsan.budi.wicaksono@students.untidar.ac.id>
2024-09-11 21:19:35 +0700

    Add files via upload
    Add Custom Code WordFrequency

---
+++ Prak3/WordFrequency/WordFrequencyMapper.java
@@ -0,0 +1,18 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+public class WordFrequencyMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String[] words = value.toString().split("\\s+");
+        for (String wordStr : words) {
+            word.set(wordStr);
+            context.write(word, one);
+        }
+    }
+}
